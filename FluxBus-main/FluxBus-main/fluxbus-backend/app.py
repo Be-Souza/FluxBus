8be8b9adfd237d7043eeaf49486144be9a96281c
Bernardo Souza <dev.besouza@gmail.com>
2025-06-03 08:58:53 -0300

    Add files via upload

---
+++ FluxBus-main/FluxBus-main/fluxbus-backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        print("E-mail recebido:", email)
+        print("Senha recebida:", senha)
+
+        return "Login recebido com sucesso!" 
+
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
